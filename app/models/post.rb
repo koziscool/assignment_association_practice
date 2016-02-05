d3ff9e5f5438d62bcdd773dfcfe23eafa7b41a99
Jeffrey Gisin <jgisin@gmail.com>
2016-02-04 18:06:22 -0700

    merge

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,10 +2,10 @@ class Post < ActiveRecord::Base
   has_many :comments
   belongs_to :category
 
-  has_many :post_authorings, class_name: 'user_post'
+  has_many :post_authorings, class_name: 'UserPost'
 
-  has_many :authors, :through => :user_posts, source: :user 
+  has_many :authors, :through => :user_posts, source: :user
 
   has_many :post_tags
-  has_many :tags, :through => :post_tags 
+  has_many :tags, :through => :post_tags
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
   has_many :authored_comments, class_name: 'Comment'
-  has_many :post_authorings, class_name: 'user_post'
+  has_many :post_authorings, :class_name => 'UserPost'
   has_many :authored_posts, :through => :post_authorings, :source => :post
 end
